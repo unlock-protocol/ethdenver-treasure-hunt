317a8ffc8c6bf8e7ae69a96f399aa5bccc1711c0
Clément Renaud <clement+git@clementrenaud.com>
2023-02-08 12:50:53 +0100

    remove hh console

--- contracts/contracts/TreasureHunt.sol
+++ contracts/contracts/TreasureHunt.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@unlock-protocol/contracts/dist/PublicLock/IPublicLockV12.sol";
 
