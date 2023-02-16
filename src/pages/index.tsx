4c6dc61c10c327693dc6543f7112175b03faf8d3
Julien Genestoux <julien.genestoux@gmail.com>
2023-02-16 18:07:33 -0500

    adding favicon and co

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -165,7 +165,29 @@ export default function Home() {
           content="Unlock Protocol invites you to join the ETHDenver treasure hunt. Participants will experience the IRL NFT experience by unlocking stops and be rewarded with a refreshing treat at the end!"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+
         <meta property="og:title" content="ETHDenver Treasure Hunt" />
         <meta property="twitter:title" content="ETHDenver Treasure Hunt" />
 
